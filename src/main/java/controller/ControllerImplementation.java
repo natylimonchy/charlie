8331ec39809426f1f3b6f51eb1e74f2b690b1588
natylimonchy <limonchynatalia@gmail.com>
2026-05-06 10:39:33 +0200

    refactor: replace hardcoded storage strings with constants #2

--- src/main/java/controller/ControllerImplementation.java
+++ src/main/java/controller/ControllerImplementation.java
@@ -37,6 +37,7 @@ import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import org.jdatepicker.DateModel;
+import utils.Constants;
 
 /**
  * This class starts the visual part of the application and programs and manages
@@ -119,22 +120,22 @@ public class ControllerImplementation implements IController, ActionListener {
         String daoSelected = ((javax.swing.JCheckBox) (dSS.getAccept()[1])).getText();
         dSS.dispose();
         switch (daoSelected) {
-            case "ArrayList":
+            case Constants.ARRAY_LIST:
                 dao = new DAOArrayList();
                 break;
-            case "HashMap":
+            case Constants.HASHMAP:
                 dao = new DAOHashMap();
                 break;
-            case "File":
+            case Constants.FILE:
                 setupFileStorage();
                 break;
-            case "File (Serialization)":
+            case Constants.FILE_SERIALIZATION:
                 setupFileSerialization();
                 break;
-            case "SQL - Database":
+            case Constants.SQL_DATABASE:
                 setupSQLDatabase();
                 break;
-            case "JPA - Database":
+            case Constants.JPA_DATABASE:
                 setupJPADatabase();
                 break;
         }
